f2180d201a875be0963de5cbcddb46405516c21e
460817183@qq.com <460817183@qq.com>
2017-01-03 11:57:16 +0800

    my first commit

---
+++ app/src/main/java/com/xqf/customradiobutton/MyCheckButton.java
@@ -0,0 +1,83 @@
+package com.xqf.customradiobutton;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+/**
+ * 可以设置图片大小的CheckBox
+ */
+
+
+public class MyCheckButton extends CheckBox {
+
+    private int mDrawableHeight;//xml文件中设置的高度
+    private int mDrawableWidth;//xml文件中设置的宽度
+
+    public MyCheckButton(Context context) {
+        this(context, null, 0);
+    }
+
+    public MyCheckButton(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyCheckButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        // TODO Auto-generated constructor stub
+        Drawable drawableLeft = null, drawableTop = null, drawableRight = null, drawableBottom = null;
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.MyRadioButton);
+        setClickable(true);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.MyRadioButton_drawableHeight:
+                    mDrawableHeight = a.getDimensionPixelSize(R.styleable.MyRadioButton_drawableHeight, 50);
+                    break;
+                case R.styleable.MyRadioButton_drawableWidth:
+                    mDrawableWidth = a.getDimensionPixelSize(R.styleable.MyRadioButton_drawableWidth, 50);
+                    break;
+                case R.styleable.MyRadioButton_drawableTop:
+                    drawableTop = a.getDrawable(attr);
+                    break;
+                case R.styleable.MyRadioButton_drawableBottom:
+                    drawableRight = a.getDrawable(attr);
+                    break;
+                case R.styleable.MyRadioButton_drawableRight:
+                    drawableBottom = a.getDrawable(attr);
+                    break;
+                case R.styleable.MyRadioButton_drawableLeft:
+                    drawableLeft = a.getDrawable(attr);
+                    break;
+            }
+        }
+        a.recycle();
+
+        setCompoundDrawablesWithIntrinsicBounds(drawableLeft, drawableTop, drawableRight, drawableBottom);
+
+    }
+
+    public void setCompoundDrawablesWithIntrinsicBounds(Drawable left,
+                                                        Drawable top, Drawable right, Drawable bottom) {
+
+        if (left != null) {
+            left.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (right != null) {
+            right.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (top != null) {
+            top.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        if (bottom != null) {
+            bottom.setBounds(0, 0, mDrawableWidth, mDrawableHeight);
+        }
+        setCompoundDrawables(left, top, right, bottom);
+    }
+}
